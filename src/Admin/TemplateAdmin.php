878a3fe1ca0aec8983a09c250383fab5fadf1cc9
Martin Georgiev <martin.georgiev@gmail.com>
2018-07-03 15:13:18 +0100

    General PHP 7.0 clean-up (add strict types, typehints, return types; remove obsolete docblocks)

--- src/Admin/TemplateAdmin.php
+++ src/Admin/TemplateAdmin.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Alpha\TwigBundle\Admin;
 
+use Alpha\TwigBundle\Entity\Template;
 use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
@@ -10,49 +13,48 @@ use Symfony\Component\Validator\Constraints\NotBlank;
 
 class TemplateAdmin extends AbstractAdmin
 {
-    // Fields to be shown on create/edit forms
-    protected function configureFormFields(FormMapper $formMapper)
+    protected function configureFormFields(FormMapper $form)
     {
-        $formMapper
+        $form
             ->add('name', 'text', [
                 'label' => 'Name',
-                'help' => 'Follow the convection: type.name.format.twig, e.g. sms.loan_declined.txt.twig',
+                'help' => 'Follow the convection: type.name.format.twig, e.g. email.welcome.txt.twig',
                 'constraints' => [
-                    new NotBlank()
+                    new NotBlank(),
                 ]
             ])
             ->add('source', 'textarea', [
                 'label' => 'Template',
                 'constraints' => [
-                    new NotBlank()
+                    new NotBlank(),
                 ]
             ]);
     }
 
-    // Fields to be shown on filter forms
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    protected function configureDatagridFilters(DatagridMapper $filter)
     {
-        $datagridMapper
+        $filter
             ->add('name')
             ->add('source');
     }
 
-    // Fields to be shown on lists
-    protected function configureListFields(ListMapper $listMapper)
+    protected function configureListFields(ListMapper $list)
     {
-        $listMapper
+        $list
             ->addIdentifier('name')
             ->add('source')
             ->add('lastModified');
     }
 
-    public function prePersist($template)
+    public function prePersist($object)
     {
-        $template->setLastModifiedDate();
+        assert($object instanceof Template);
+        $object->setLastModifiedDate();
     }
 
-    public function preUpdate($template)
+    public function preUpdate($object)
     {
-        $template->setLastModifiedDate();
+        assert($object instanceof Template);
+        $object->setLastModifiedDate();
     }
 }
